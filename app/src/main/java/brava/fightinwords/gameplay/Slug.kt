195cb4eee339f5bc30e0c72160e4abdbf0685073
BrandonCimino <brandon.cimino@gmail.com>
2025-06-08 16:28:57 -0500

    Passed a reference to a `Slug` for its `Typesetter`

--- app/src/main/java/brava/fightinwords/gameplay/Slug.kt
+++ app/src/main/java/brava/fightinwords/gameplay/Slug.kt
@@ -1,10 +1,13 @@
 package brava.fightinwords.gameplay
 
-import brava.fightinwords.Letter
-
 /**
  * A physical [brava.fightinwords.Letter] object that can be moved around by a [Typesetter].
  *
  * > It might be more accurate to call this a [sort](https://en.wikipedia.org/wiki/Sort_(typesetting)), but that is needlessly confusing in the context of programming.
  */
-class Slug(val letter: Letter)
+class Slug(val letter: Char, val myTypesetter: Typesetter) {
+    val isSlotted: Boolean
+        get() {
+            return myTypesetter.galley.contains(this)
+        }
+}
--- app/src/main/java/brava/fightinwords/gameplay/Typesetter.kt
+++ app/src/main/java/brava/fightinwords/gameplay/Typesetter.kt
@@ -1,18 +1,20 @@
 package brava.fightinwords.gameplay
 
 import brava.fightinwords.Letter
+import brava.fightinwords.gameplay.Phonology.Companion.englishPhonology
 import kotlin.random.Random
 
 class Typesetter(
     /**
      * The original [Slug]s that the game was started with.
      */
-    val progenitorPool: List<Slug>
+    val progenitorPool: List<Char>
 ) {
     /**
      * The selectable letters that are being played with, in the order that they are visible to the player.
      */
     var currentPool: List<Slug> = progenitorPool
+        .map { Slug(it, this) }
         private set
 
     /**
@@ -28,12 +30,12 @@ class Typesetter(
     }
 
     fun sortPhonologically() {
-        currentPool = progenitorPool.sortedBy { it.letter.phonology }
+        currentPool = currentPool.sortedBy { it.letter.englishPhonology }
         currentSorting = LetterSorting.Phonological
     }
 
     fun sortAlphabetically() {
-        currentPool = progenitorPool.sortedBy { it.letter }
+        currentPool = currentPool.sortedBy { it.letter }
         currentSorting = LetterSorting.Alphabetical
     }
 
@@ -51,5 +53,9 @@ class Typesetter(
 
     fun submitAndClear(): List<Letter> {
         return galley.submitAndClear()
+    }
+
+    fun Slug.isSlotted() {
+        galley.contains(this)
     }
 }

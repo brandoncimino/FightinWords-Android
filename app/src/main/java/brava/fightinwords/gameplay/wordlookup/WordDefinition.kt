2d27af25006f90cb81afd03b6b0c7268d72a19c5
BrandonCimino <brandon.cimino@gmail.com>
2025-07-06 18:50:20 -0500

    WordDefinition.kt

---
+++ app/src/main/java/brava/fightinwords/gameplay/wordlookup/WordDefinition.kt
@@ -0,0 +1,8 @@
+package brava.fightinwords.gameplay.wordlookup
+
+data class WordDefinition(
+    val word: String,
+    val language: String,
+    val partOfSpeech: String,
+    val definition: String
+)
--- app/src/main/java/brava/fightinwords/gameplay/wordlookup/WordLookup.kt
+++ app/src/main/java/brava/fightinwords/gameplay/wordlookup/WordLookup.kt
@@ -1,5 +1,9 @@
 package brava.fightinwords.gameplay.wordlookup
 
+import kotlin.random.Random
+
 interface WordLookup {
     abstract fun isWord(word: String): Boolean;
+
+    abstract fun findRandomWord(desiredLength: Int, random: Random): Result<String>;
 }

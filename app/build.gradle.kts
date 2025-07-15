8e3592a00279edb45d153ce5b5a9d0ce57e43142
BrandonCimino <brandon.cimino@gmail.com>
2025-07-15 18:04:48 -0500

    Dependencies, perhaps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,14 +38,15 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation(platform("androidx.compose:compose-bom:2024.09.00"))
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation(platform("androidx.compose:compose-bom:2025.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.guava:guava:33.4.8-android")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.assertj:assertj-core:4.0.0-M1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")

5ed3d96798e9a0aa327f4b24fe7837ca312e395f
BrandonCimino <brandon.cimino@gmail.com>
2025-07-27 23:43:11 -0500

    Simplified and (possibly) optimized

--- app/src/main/java/brava/fightinwords/gameplay/LetterSorting.kt
+++ app/src/main/java/brava/fightinwords/gameplay/LetterSorting.kt
@@ -2,11 +2,7 @@ package brava.fightinwords.gameplay
 
 import brava.fightinwords.gameplay.Phonology.Companion.englishPhonology
 
-enum class LetterSorting(val comparator: Comparator<Char>) : Comparator<Char> {
+enum class LetterSorting(val comparator: Comparator<Char>) : Comparator<Char> by comparator {
     Alphabetical(Comparator.naturalOrder()),
-    Phonological(Comparator.comparing { it.englishPhonology });
-
-    override fun compare(o1: Char, o2: Char): Int {
-        return comparator.compare(o1, o2)
-    }
+    Phonological(Comparator.comparingInt { it.englishPhonology.ordinal });
 }

2edf5ce72d4b436dc416c3531bb4f4ba4eeeb96e
BrandonCimino <brandon.cimino@gmail.com>
2025-08-05 18:55:39 -0500

    Removed more unused stuff

--- app/src/main/java/brava/fightinwords/ui/typesetter/TypesetterButtons.kt
+++ app/src/main/java/brava/fightinwords/ui/typesetter/TypesetterButtons.kt
@@ -1,37 +1,8 @@
 package brava.fightinwords.ui.typesetter
 
-import brava.fightinwords.gameplay.Typesetter
-import brava.fightinwords.ui.typesetter.SortButton.Companion.clickSortButton
-
 data class TypesetterButtons(
     val onSortButtonClick: (SortButton) -> Unit,
     val onLetterButtonClick: (index: Int) -> Unit,
     val onGalleyButtonClick: (index: Int) -> Unit,
     val onSubmit: () -> Unit,
-) {
-    fun then(action: () -> Unit): TypesetterButtons {
-        return TypesetterButtons(
-            onSortButtonClick = { onSortButtonClick(it); action() },
-            onLetterButtonClick = { onLetterButtonClick(it); action() },
-            onGalleyButtonClick = { onGalleyButtonClick(it); action() },
-            onSubmit = { onSubmit(); action() }
-        )
-    }
-
-    companion object {
-        fun clickSortButton(sortButton: SortButton, typesetter: Typesetter) {
-            typesetter.clickSortButton(sortButton)
-        }
-    }
-}
-
-fun Typesetter.buttons(
-    onSubmit: () -> Unit
-): TypesetterButtons {
-    return TypesetterButtons(
-        onSortButtonClick = { clickSortButton(it) },
-        onLetterButtonClick = this::toggle,
-        onGalleyButtonClick = { this.toggle(this.galley.get(it)) },
-        onSubmit = onSubmit
-    )
-}
+)

475c6e2d62c2304ed2fcc1bdf50ea2f38d2d7d86
BrandonCimino <brandon.cimino@gmail.com>
2025-08-07 14:46:44 -0500

    disabled dynamic color

--- app/src/main/java/brava/fightinwords/ui/theme/Theme.kt
+++ app/src/main/java/brava/fightinwords/ui/theme/Theme.kt
@@ -1,13 +1,8 @@
 package brava.fightinwords.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
@@ -37,8 +32,8 @@ private val LightColorScheme = lightColorScheme(
 fun FightinWordsTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    dynamicColor: Boolean = false,
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {

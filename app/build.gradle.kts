5f410862251e7aa40e67ea1d6e714819db9d8516
BrandonCimino <brandon.cimino@gmail.com>
2025-08-07 22:20:35 -0500

    Ran fancy upgrade stuff

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "brava.fightinwords"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "brava.fightinwords"
@@ -28,9 +28,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
     }
     buildFeatures {
         compose = true

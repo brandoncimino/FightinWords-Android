ce97d55c6d2c59bd815bc5e75ee69f6ab27a5a0c
BrandonCimino <brandon.cimino@gmail.com>
2025-08-07 23:37:31 -0500

    Upgrade to Java 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,13 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+        }
     }
     buildFeatures {
         compose = true

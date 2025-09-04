2fd811b5abd4af3fed5dd420cd2447573fd8ce6e
BCimino_CCCIS <bcimino@cccis.com>
2025-09-04 12:46:36 -0500

    Used the new `.utf8()` extension method

--- app/src/main/java/brava/fightinwords/gameplay/wordlookup/DefinitionsCsvLookup.kt
+++ app/src/main/java/brava/fightinwords/gameplay/wordlookup/DefinitionsCsvLookup.kt
@@ -1,21 +1,19 @@
 package brava.fightinwords.gameplay.wordlookup
 
+import brava.fightinwords.botlin.utf8
 import brava.fightinwords.gameplay.KnownLanguage
 import brava.fightinwords.gameplay.wordlookup.WordLookupHelpers.extractTinyWordFromLineStart
 import java.io.File
 import java.nio.ByteBuffer
-import java.nio.charset.StandardCharsets
 
 class DefinitionsCsvLookup private constructor(
-    val entries: LongMappedLines,
+    entries: LongMappedLines,
     private val language: KnownLanguage = KnownLanguage.English,
 ) : MemoryMappedDefinitionLookup(entries) {
     constructor(file: File) : this(parseWordLineRanges(file))
 
     override fun parseLine(line: ByteBuffer): WordDefinition {
-        // TODO: Replace this with something that fails if the bytes aren't well-formed
-        val lineString = StandardCharsets.UTF_8.decode(line)
-        return WordLookupHelpers.parseDefinitionCsvLine(lineString, language)
+        return WordLookupHelpers.parseDefinitionCsvLine(line.utf8(), language)
     }
 
     companion object {
---
+++ app/src/test/java/brava/fightinwords/gameplay/wordlookup/DefinitionsCsvLookupTest.kt
@@ -0,0 +1,48 @@
+package brava.fightinwords.gameplay.wordlookup
+
+import brava.fightinwords.gameplay.KnownLanguage
+import brava.fightinwords.gameplay.data.Word.Companion.toWord
+import org.assertj.core.api.Assertions
+import org.junit.Test
+import java.nio.file.Files
+
+class DefinitionsCsvLookupTest {
+    val firstFewLines: String = """
+        aaagh,Interjection,0,Elongated form of agh.
+        aaargh,Interjection,0,Alternative form of argh!
+        aaawesome,Adjective,0,Elongated form of awesome.
+        aabomycin,Noun,0,venturicidin
+        aabomycins,Noun,0,plural of aabomycin
+        aad,Adjective,0,(Geordie) old
+        aads,Noun,0,plural of aad
+        aagh,Interjection,0,An exclamation of horror, disgust or frustration
+        aah,Interjection,1,Indication of amazement or surprise or enthusiasm.
+        aahed,Verb,1,simple past and past participle of aah
+        aaher,Noun,0,One who aahs (in various senses).
+        aahing,Verb,1,present participle and gerund of aah
+        aahings,Noun,0,plural of aahing
+        aahs,Noun,1,plural of aah
+    """.trimIndent()
+
+    @Test
+    fun fromDefinitionsCsvFile() {
+        val tempFile = Files.createTempFile(null,null)
+        Files.write(tempFile, firstFewLines.lines())
+        val lookup = DefinitionsCsvLookup(tempFile.toFile())
+
+        val word = "aabomycins".toWord()
+        val found = lookup.findDefinition(word)
+        println("found = ${found}")
+
+        Assertions.assertThat(found)
+            .isEqualTo(
+                WordDefinition(
+                    word,
+                    KnownLanguage.English,
+                    "Noun",
+                    "plural of aabomycin",
+                    false
+                )
+            )
+    }
+}

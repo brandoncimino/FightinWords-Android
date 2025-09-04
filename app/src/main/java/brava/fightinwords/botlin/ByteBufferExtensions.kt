31d822c44fbb3f8857fab0d1283007459985c7b3
BCimino_CCCIS <bcimino@cccis.com>
2025-09-04 12:37:03 -0500

    Renamed to `ByteBufferExtensions`

---
+++ app/src/main/java/brava/fightinwords/botlin/ByteBufferExtensions.kt
@@ -0,0 +1,58 @@
+package brava.fightinwords.botlin
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.nio.ByteBuffer
+import java.nio.CharBuffer
+import java.nio.charset.CharsetDecoder
+import java.nio.charset.StandardCharsets
+
+@RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+internal inline fun ByteBuffer.forEachLine(
+    action: (ByteBuffer) -> Unit,
+) {
+    forEachLineRange { start, endInclusive ->
+        val line = this.slice(start, (endInclusive - start) + 1)
+        action(line)
+    }
+}
+
+inline fun ByteBuffer.forEachLineRange(
+    action: (start: Int, endInclusive: Int) -> Unit,
+) {
+    var lineStart = 0
+    var pos = 0
+    while (pos < limit()) {
+        val current = get(pos)
+        if (current == '\n'.code.toByte()) {
+            action(lineStart, pos - 1)
+            lineStart = pos + 1
+        }
+
+        pos += 1
+    }
+
+    action(lineStart, limit() - 1)
+}
+
+fun ByteBuffer.utf8() : CharBuffer {
+    // TODO: Do this in a way that will:
+    //   - Throw errors for non-UTF8 bytes, instead of `UTF_8.decode()`, which silently replaces them with garbage
+    //   - Is thread-safe, unlike storing a configured `UTF_8.newDecoder()`
+    //   - Won't cause unnecessary allocations, unlike calling `UTF_8.newDecoder()` every time
+    //   - Works on `ByteBuffer`s, unlike `Utf8.isWellFormed()`
+    //  The current implementation is based on `UTF_8.decode()`'s internal optimization.
+    //  If I really wanted to waste my time on the tiniest optimization, I could maybe use a pool of decoders and rent them, but I mean...come on.
+    return threadLocalUtf8Decoder.get()!!.decode(this)
+}
+
+private val threadLocalUtf8Decoder : ThreadLocal<CharsetDecoder> = ThreadLocal.withInitial { StandardCharsets.UTF_8.newDecoder() }
+inline fun ByteBuffer.indexOf(predicate: (Byte) -> Boolean, startIndex: Int = 0) : Int {
+    for(i in startIndex..limit()){
+        if(predicate(get(i))){
+            return i
+        }
+    }
+
+    return -1
+}

6aedbc50ad31eec75ce94ab3de04e98f8182dd82
BCimino_CCCIS <bcimino@cccis.com>
2025-09-06 18:15:59 -0500

    Switched to operate on `Letter`s

--- app/src/main/java/brava/fightinwords/gameplay/LetterSorting.kt
+++ app/src/main/java/brava/fightinwords/gameplay/LetterSorting.kt
@@ -1,8 +1,9 @@
 package brava.fightinwords.gameplay
 
 import brava.fightinwords.gameplay.Phonology.Companion.englishPhonology
+import brava.fightinwords.gameplay.data.Letter
 
-enum class LetterSorting(val comparator: Comparator<Char>) : Comparator<Char> by comparator {
+enum class LetterSorting(val comparator: Comparator<Letter>) : Comparator<Letter> by comparator {
     Alphabetical(Comparator.naturalOrder()),
     Phonological(Comparator.comparingInt { it.englishPhonology.ordinal });
 }

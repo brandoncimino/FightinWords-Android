e7480741dfdc4126856ea47c71cdae1616ec7114
BCimino_CCCIS <bcimino@cccis.com>
2025-10-26 19:09:23 -0500

    Added `SharedResources` for constructing employees

--- app/src/main/java/brava/fightinwords/gameplay/hr/EmployeeFactory.kt
+++ app/src/main/java/brava/fightinwords/gameplay/hr/EmployeeFactory.kt
@@ -2,6 +2,11 @@ package brava.fightinwords.gameplay.hr
 
 import brava.fightinwords.SaveGameState
 import brava.fightinwords.gameplay.GamePlan
+import brava.fightinwords.gameplay.data.LetterPool
+import brava.fightinwords.gameplay.data.WordPool
+import brava.fightinwords.gameplay.wordlookup.Factotum
+import brava.fightinwords.gameplay.wordlookup.Factotum.Companion.getFactotum
+import brava.fightinwords.gameplay.wordlookup.WordSourceLoader
 
 interface EmployeeFactory<
         EMPLOYEE,
@@ -9,13 +14,31 @@ interface EmployeeFactory<
         > {
     fun EMPLOYEE.getSerializableState(): SERIALIZABLE_STATE
 
-    fun fromSerializableState(state: SERIALIZABLE_STATE, gamePlan: GamePlan): EMPLOYEE
+    fun fromSerializableState(state: SERIALIZABLE_STATE, sharedResources: SharedResources): EMPLOYEE
 
     fun SaveGameState.getEmployeeState(): SERIALIZABLE_STATE;
 
     companion object {
-        fun <E, S> EmployeeFactory<E, S>.load(saveGameState: SaveGameState): E {
-            return fromSerializableState(saveGameState.getEmployeeState(), saveGameState.gamePlan)
+        fun <E, S> EmployeeFactory<E, S>.load(
+            saveGameState: SaveGameState,
+            sharedResources: SharedResources,
+        ): E {
+            return fromSerializableState(saveGameState.getEmployeeState(), sharedResources)
         }
+    }
+
+    data class SharedResources(
+        private val wordSourceLoader: WordSourceLoader,
+        val gamePlan: GamePlan,
+    ) {
+        val letterPool: LetterPool = LetterPool(gamePlan.letterPool)
+
+        val factotum: Factotum = wordSourceLoader.getFactotum(gamePlan)
+
+        val coreWordPool: WordPool = WordPool(
+            letterPool,
+            factotum.coreWordList,
+            gamePlan.minimumWordLength
+        )
     }
 }

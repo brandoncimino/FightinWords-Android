185541b8ea6f250361fd3cd536b246318ad180eb
BCimino_CCCIS <bcimino@cccis.com>
2025-10-26 19:12:07 -0500

    added the different word lists and lookups to the `GamePlan`

--- app/src/main/java/brava/fightinwords/gameplay/GamePlan.kt
+++ app/src/main/java/brava/fightinwords/gameplay/GamePlan.kt
@@ -2,6 +2,10 @@ package brava.fightinwords.gameplay
 
 import brava.fightinwords.gameplay.data.Word
 import brava.fightinwords.gameplay.scoring.Ledgerman
+import brava.fightinwords.gameplay.scoring.WordScoringStrategy
+import brava.fightinwords.gameplay.wordlookup.WordList
+import brava.fightinwords.gameplay.wordlookup.WordLookup
+import brava.fightinwords.gameplay.wordlookup.WordSource
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -11,4 +15,16 @@ data class GamePlan(
     val minimumWordLength: Int = 3,
     val wordLanguage: KnownLanguage = KnownLanguage.English,
     val scoreboardSorting: Ledgerman.WordSorting = Ledgerman.WordSorting.LengthFirst,
+
+    /**
+     * The [brava.fightinwords.gameplay.wordlookup.WordList] that defines my [WordCategory.Core] words.
+     */
+    val coreWordList: WordList.Id = WordSource.NaspaWordList2023,
+
+    /**
+     * Places where we can check for [WordCategory.Bonus] words.
+     */
+    val bonusWordLookups: List<WordLookup.Id> = listOf(),
+
+    val scoringStrategy: WordScoringStrategy = WordScoringStrategy.Scrabble,
 )

a1f01fa605d9411d91500f37046a87b18ed5c82a
BCimino_CCCIS <bcimino@cccis.com>
2025-10-28 16:17:30 -0500

    Added `assertRoundTrip` extensions

--- app/src/test/java/brava/fightinwords/Besting.kt
+++ app/src/test/java/brava/fightinwords/Besting.kt
@@ -1,5 +1,12 @@
 package brava.fightinwords
 
+import kotlinx.serialization.BinaryFormat
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerialFormat
+import kotlinx.serialization.StringFormat
+import kotlinx.serialization.decodeFromHexString
+import kotlinx.serialization.encodeToHexString
+import kotlinx.serialization.serializer
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.IterableAssert
 import org.assertj.core.api.ObjectAssert
@@ -29,6 +36,68 @@ object Besting {
     }
 
     fun <T> T.ass(nickname: String) = Assertions.assertThat(this).`as`(nickname)
+
+    inline fun <reified T, reified S> T.assertRoundTrip(
+        serializer: (T) -> S,
+        deserializer: (S) -> T,
+    ) {
+        println(
+            """
+>> Round trip of 
+   -➡ ${T::class.simpleName} 
+    ↳ ${S::class.simpleName}  
+    ↳ ${T::class.simpleName}  
+"""
+        )
+
+        val serialized = serializer(this)
+
+        println(
+            "serialized = [${serialized::class}] ${
+                when (serialized) {
+                    is ByteArray -> serialized.toList()
+                    else         -> serialized
+                }
+            }"
+        )
+
+        val deserialized = deserializer(serialized)
+        println("deserialized = [${deserialized?.javaClass}] ${deserialized}")
+
+        println("obj == deserialized = ${this == deserialized}")
+
+        Assertions.assertThat(deserialized)
+            .isInstanceOf(T::class.java)
+            .isEqualTo(this)
+            .usingRecursiveAssertion()
+            .isEqualTo(this)
+    }
+
+    inline fun <reified T> T.assertRoundTrip(
+        serialFormat: SerialFormat,
+        serializer: KSerializer<T> = serializer<T>(),
+    ) {
+        return when (serialFormat) {
+            is StringFormat -> assertRoundTrip(
+                { serialFormat.encodeToString(serializer, it) },
+                { serialFormat.decodeFromString(serializer, it) }
+            )
+
+            is BinaryFormat -> {
+                assertRoundTrip(
+                    { serialFormat.encodeToByteArray(serializer, it) },
+                    { serialFormat.decodeFromByteArray(serializer, it) }
+                )
+
+                assertRoundTrip(
+                    { serialFormat.encodeToHexString(serializer, it) },
+                    { serialFormat.decodeFromHexString(serializer, it) }
+                )
+            }
+
+            else            -> throw UnsupportedOperationException()
+        }
+    }
 }
 
 data class Nicknamed<T>(val value: T, val nickname: String) {

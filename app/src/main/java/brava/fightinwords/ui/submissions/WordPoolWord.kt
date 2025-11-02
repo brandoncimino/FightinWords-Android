3f6ca3c4b344350a0f9b379d0cd27d543fee9944
BCimino_CCCIS <bcimino@cccis.com>
2025-11-02 15:11:45 -0600

    used new submission types

--- app/src/main/java/brava/fightinwords/ui/submissions/WordPoolWord.kt
+++ app/src/main/java/brava/fightinwords/ui/submissions/WordPoolWord.kt
@@ -6,46 +6,62 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
-import brava.fightinwords.gameplay.Accepted
-import brava.fightinwords.gameplay.DefinedWordState
-import brava.fightinwords.gameplay.WordState
+import androidx.compose.ui.unit.dp
+import brava.fightinwords.gameplay.WordCategory
+import brava.fightinwords.gameplay.data.Word
+import brava.fightinwords.gameplay.data.Word.Companion.indices
+import brava.fightinwords.gameplay.scoring.ScoreboardWord
+import brava.fightinwords.gameplay.scoring.ScoreboardWordVisibility
+import brava.fightinwords.ui.PreviewHelpers.deez
 import brava.fightinwords.ui.typesetter.LetterTile
 import brava.fightinwords.ui.typesetter.LetterTileFlavor
 
 @Composable
 fun WordPoolWord(
-    wordState: WordState,
+    wordState: ScoreboardWord,
     letterPersonalSpace: Dp,
     modifier: Modifier = Modifier,
     horizontalArrangement: Arrangement.Horizontal = Arrangement.Start,
-    onClick: (WordState) -> Unit = {},
+    onClick: (Word) -> Unit = {},
 ) {
     val word = wordState.word
 
     Row(
         modifier = modifier
-            .clickable {
-                onClick(wordState)
+            .clickable(wordState.visibility == ScoreboardWordVisibility.Full) {
+                onClick(word)
             }
             .requiredHeight(letterPersonalSpace),
         horizontalArrangement = horizontalArrangement
     ) {
-        val flavor = when (wordState is DefinedWordState && wordState.wordDefinition.isNaspaWord) {
-            true -> LetterTileFlavor.Submission
-            false -> LetterTileFlavor.BonusWord
+
+        val flavor = when (wordState.category) {
+            WordCategory.Bonus -> LetterTileFlavor.BonusWord
+            WordCategory.Core  -> LetterTileFlavor.Submission
         }
-        for (letter in word) {
+
+        for (letter in word.indices) {
             LetterTile(
-                letter = when (wordState) {
-                    is Accepted -> letter.toString()
-                    else        -> " "
+                letter = when (wordState.visibility) {
+                    ScoreboardWordVisibility.Full   -> word[letter].toString()
+                    ScoreboardWordVisibility.Masked -> " "
                 },
                 flavor = flavor,
                 personalSpace = letterPersonalSpace,
-                onClick = { onClick(wordState) },
-                buttonEnabled = wordState is Accepted
+                onClick = { onClick(word) },
+                buttonEnabled = wordState.visibility != ScoreboardWordVisibility.Masked
             )
         }
     }
+}
+
+@Preview
+@Composable
+fun WordPoolWordPreview() {
+    WordPoolWord(
+        ScoreboardWord(deez.word, ScoreboardWordVisibility.Full, WordCategory.Bonus),
+        letterPersonalSpace = 10.dp
+    )
 }

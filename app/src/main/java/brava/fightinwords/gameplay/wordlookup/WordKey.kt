c60a9afdaa50a13cab95b5c51675413b8e9fe16c
BCimino_CCCIS <bcimino@cccis.com>
2025-11-02 15:10:49 -0600

    didn't I commit this like 20 times already

--- app/src/main/java/brava/fightinwords/gameplay/wordlookup/WordKey.kt
+++ app/src/main/java/brava/fightinwords/gameplay/wordlookup/WordKey.kt
@@ -1,9 +1,8 @@
 package brava.fightinwords.gameplay.wordlookup
 
-import brava.fightinwords.gameplay.data.TinyWord
 import brava.fightinwords.gameplay.data.Word
 
 data class WordKey(
     val word: Word,
-    val partOfSpeech: TinyWord
+    val partOfSpeech: PartOfSpeech? = null,
 )

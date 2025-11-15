e0c6d6042df56b5b00ec1e14247345a37869205f
BCimino_CCCIS <bcimino@cccis.com>
2025-11-15 16:56:39 -0600

    added `toImmutableList` and `toImmutableSet` extensions

--- app/src/main/java/brava/fightinwords/botlin/GuavaImmutableExtensions.kt
+++ app/src/main/java/brava/fightinwords/botlin/GuavaImmutableExtensions.kt
@@ -1,6 +1,7 @@
 package brava.fightinwords.botlin
 
 import com.google.common.collect.ImmutableList
+import com.google.common.collect.ImmutableSet
 
 /**
  * @return A new [ImmutableList] containing [this] and one additional [element].
@@ -29,4 +30,18 @@ operator fun <T : Any> ImmutableList<T>.plus(elements: Iterable<T>): ImmutableLi
         .addAll(this)
         .addAll(elements)
         .build()
+}
+
+/**
+ * @see ImmutableList.copyOf
+ */
+fun <T : Any> Sequence<T>.toImmutableList(): ImmutableList<T> {
+    return ImmutableList.copyOf(iterator())
+}
+
+/**
+ * @see ImmutableSet.copyOf
+ */
+fun <T : Any> Sequence<T>.toImmutableSet(): ImmutableSet<T> {
+    return ImmutableSet.copyOf(iterator())
 }
